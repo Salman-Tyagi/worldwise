fbed0c95201b753f3153c3c0a84c702e47c7498d
Salman Tyagi <salmantyagi15@gmail.com>
2023-09-10 11:51:03 +0530

    initialized dynamic route with url parameters

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,15 +1,18 @@
+import { Link } from 'react-router-dom';
 import styles from './CityItem.module.css';
 
 /* eslint-disable react/prop-types */
 export default function CityItem({ city }) {
-  const { cityName, date, emoji } = city;
+  const { cityName, date, emoji, id } = city;
 
   return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
-      <p className={styles.name}>{cityName}</p>
-      <p className={styles.date}>({new Date(date).toDateString()})</p>
-      <button className={styles.deleteBtn}>&times;</button>
+    <li>
+      <Link to={`${id}`} className={styles.cityItem}>
+        <span className={styles.emoji}>{emoji}</span>
+        <p className={styles.name}>{cityName}</p>
+        <p className={styles.date}>({new Date(date).toDateString()})</p>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   );
 }
