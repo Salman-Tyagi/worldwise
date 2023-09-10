5f457371f260e4a7c722be806aa2997ec15b0c2e
Salman Tyagi <salmantyagi15@gmail.com>
2023-09-10 12:30:30 +0530

    implemented: get state from useSearchParams

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -3,11 +3,12 @@ import styles from './CityItem.module.css';
 
 /* eslint-disable react/prop-types */
 export default function CityItem({ city }) {
-  const { cityName, date, emoji, id } = city;
+  const { cityName, date, emoji, id, position } = city;
+  const { lat, lng } = position;
 
   return (
     <li>
-      <Link to={`${id}`} className={styles.cityItem}>
+      <Link to={`${id}?lat=${lat}&lng=${lng}`} className={styles.cityItem}>
         <span className={styles.emoji}>{emoji}</span>
         <p className={styles.name}>{cityName}</p>
         <p className={styles.date}>({new Date(date).toDateString()})</p>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,18 @@
+import { useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
 export default function Map() {
-  return <div className={styles.mapContainer}>MAP</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>MAP</h1>
+      <h1>{`${lat}, ${lng}`}</h1>
+      <button onClick={() => setSearchParams({ lat: 20, lng: 40 })}>
+        Change position
+      </button>
+    </div>
+  );
 }
