8882ace804aed30f73c09dc1bd4f4bcdb58c4fb3
Salman Tyagi <salmantyagi15@gmail.com>
2023-09-11 19:18:59 +0530

    implemented: city descriptions using context api

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,14 +1,21 @@
 import { Link } from 'react-router-dom';
 import styles from './CityItem.module.css';
+import { useCitites } from '../contexts/CititesContext';
 
 /* eslint-disable react/prop-types */
 export default function CityItem({ city }) {
   const { cityName, date, emoji, id, position } = city;
   const { lat, lng } = position;
+  const { currentCity } = useCitites();
 
   return (
     <li>
-      <Link to={`${id}?lat=${lat}&lng=${lng}`} className={styles.cityItem}>
+      <Link
+        to={`${id}?lat=${lat}&lng=${lng}`}
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles['cityItem--active'] : ''
+        }`}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <p className={styles.name}>{cityName}</p>
         <p className={styles.date}>({new Date(date).toDateString()})</p>
--- src/contexts/CititesContext.jsx
+++ src/contexts/CititesContext.jsx
@@ -8,6 +8,7 @@ const CitiesContext = createContext();
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(() => {
     const fetchCities = async () => {
@@ -26,9 +27,22 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch (err) {
+      throw new Error('Something wrong in getting city data...');
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     // 2. SET VALUES FOR THE CONTEXT API
-    <CitiesContext.Provider value={{ cities, isLoading }}>
+    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
       {children}
     </CitiesContext.Provider>
   );
